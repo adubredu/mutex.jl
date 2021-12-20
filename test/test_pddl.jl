96b5b62f815aaf61857212375e79571a312b82b7
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2021-12-20 04:26:39 -0500

    test updates

--- test/test_pddl.jl
+++ test/test_pddl.jl
@@ -5,6 +5,5 @@ using mutex
 domain = load_domain("test/pddl/domain.pddl")
 problem = load_problem("test/pddl/problem.pddl")
 
-graph = create_graph(domain, problem; max_levels=1000)
-# acts = get_all_actions(domain, problem)
-1
+graph = create_graph(domain, problem; max_levels=1000);
+# acts = get_all_actions(domain, problem) 
